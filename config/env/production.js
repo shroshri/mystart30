8fc86f4fe450dfb519002315fcc7ad94efd4f554
shroshri <shroshri@gmail.com>
2015-03-30 00:41:46 +1100

    Mean App 30 days effort

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/starterapp',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://username:password@ds061258.mongolab.com:61258/30dayeffort',
 	assets: {
 		lib: {
 			css: [
